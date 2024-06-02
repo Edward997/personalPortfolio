bd5b725a15ff6ff0af6776740e29b341a94f5570
Edward997 <edward.navarrete7@gmail.com>
2024-06-02 10:56:30 -0600

    adding sections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+// Importa tus componentes de las páginas
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+];
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  icons = [
+    { path: 'assets/icons/React.png', alt: 'React', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
+    // Agrega los demás iconos aquí con sus respectivas rutas, texto alternativo y clases de estilo
+    { path: 'assets/icons/Angular.png', alt: 'Angular', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
+    { path: 'assets/icons/NET core.png', alt: 'NET Core', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
+    { path: 'assets/icons/chakra-ui.svg', alt: 'Chakra UI', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
+  ];
 }
