f71355a6241a9663707decee5791d8796fe1dbd6
Edward997 <edward.navarrete7@gmail.com>
2024-06-12 19:14:50 -0600

    adding icons

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -15,5 +15,18 @@ export class AboutComponent {
     { path: 'assets/icons/Angular.png', alt: 'Angular', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
     { path: 'assets/icons/NET core.png', alt: 'NET Core', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
     { path: 'assets/icons/chakra-ui.svg', alt: 'Chakra UI', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
+    { path: 'assets/icons/HTML5.png', alt: 'HTML', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
+    { path: 'assets/icons/CSS3.png', alt: 'CSS', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
+    { path: 'assets/icons/TypeScript.png', alt: 'TypeScript', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
+    { path: 'assets/icons/JavaScript.png', alt: 'JavaScript', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
+
+    { path: 'assets/icons/Python.png', alt: 'Python', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
+    { path: 'assets/icons/Java.png', alt: 'Java', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
+    { path: 'assets/icons/CSharp.png', alt: 'C#', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
+    { path: 'assets/icons/Postman.png', alt: 'Postman', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
+    { path: 'assets/icons/MySQL.png', alt: 'MySQL', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
+    { path: 'assets/icons/Git.png', alt: 'Git', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
+    { path: 'assets/icons/Figma.png', alt: 'Figma', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
+    { path: 'assets/icons/Visual Studio Code.png', alt: 'Visual Studio Code', className: 'h-6 w-6 text-gray-500 dark:text-gray-400' },
   ];
 }
